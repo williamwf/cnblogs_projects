ffd5b1306e0a6af5a96b5592013674d00dd21c9d
williamz <wufan_zhao@163.com>
2018-08-13 16:26:03 +0800

    updated 180813

---
+++ python/advance_py.py
@@ -0,0 +1,102 @@
+####################################################
+## 1. 类
+####################################################
+
+
+# 定义一个继承object的类
+class Human(object):
+
+    # 类属性，被所有此类的实例共用。
+    species = "H. sapiens"
+
+    # 构造方法，当实例被初始化时被调用。注意名字前后的双下划线，这是表明这个属
+    # 性或方法对Python有特殊意义，但是允许用户自行定义。你自己取名时不应该用这
+    # 种格式。
+    def __init__(self, name):
+        # Assign the argument to the instance's name attribute
+        self.name = name
+
+    # 实例方法，第一个参数总是self，就是这个实例对象
+    def say(self, msg):
+        return "{name}: {message}".format(name=self.name, message=msg)
+
+    # 类方法，被所有此类的实例共用。第一个参数是这个类对象。
+    @classmethod
+    def get_species(cls):
+        return cls.species
+
+    # 静态方法。调用时没有实例或类的绑定。
+    @staticmethod
+    def grunt():
+        return "*grunt*"
+
+
+# 构造一个实例
+i = Human(name="Ian")
+print(i.say("hi"))     # 印出 "Ian: hi"
+
+j = Human("Joel")
+print(j.say("hello"))  # 印出 "Joel: hello"
+
+# 调用一个类方法
+i.get_species()   # => "H. sapiens"
+
+# 改一个共用的类属性
+Human.species = "H. neanderthalensis"
+i.get_species()   # => "H. neanderthalensis"
+j.get_species()   # => "H. neanderthalensis"
+
+# 调用静态方法
+Human.grunt()   # => "*grunt*"
+
+
+####################################################
+## 2. 高级用法
+####################################################
+
+# 用生成器(generators)方便地写惰性运算
+def double_numbers(iterable):
+    for i in iterable:
+        yield i + i
+
+# 生成器只有在需要时才计算下一个值。它们每一次循环只生成一个值，而不是把所有的
+# 值全部算好。
+#
+# range的返回值也是一个生成器，不然一个1到900000000的列表会花很多时间和内存。
+#
+# 如果你想用一个Python的关键字当作变量名，可以加一个下划线来区分。
+range_ = range(1, 900000000)
+# 当找到一个 >=30 的结果就会停
+# 这意味着 `double_numbers` 不会生成大于30的数。
+for i in double_numbers(range_):
+    print(i)
+    if i >= 30:
+        break
+
+
+# 装饰器(decorators)
+# 这个例子中，beg装饰say
+# beg会先调用say。如果返回的say_please为真，beg会改变返回的字符串。
+from functools import wraps
+
+
+def beg(target_function):
+    @wraps(target_function)
+    def wrapper(*args, **kwargs):
+        msg, say_please = target_function(*args, **kwargs)
+        if say_please:
+            return "{} {}".format(msg, "Please! I am poor :(")
+        return msg
+
+    return wrapper
+
+
+@beg
+def say(say_please=False):
+    msg = "Can you buy me a beer?"
+    return msg, say_please
+
+
+print(say())  # Can you buy me a beer?
+print(say(say_please=True))  # Can you buy me a beer? Please! I am poor :(
+
--- python/basic_py.py
+++ python/basic_py.py
@@ -1,5 +1,8 @@
-# 版本Python 3.7
+# 版本Python 3.6
+
+####################################################
 ## 1. 原始数据类型和运算符
+####################################################
 
 # 模除
 7 % 3 # => 1
@@ -16,9 +19,242 @@
 #=> "Jack be nimble, Jack be quick, Jack jump over the candle stick"
 
 
+####################################################
+## 2. 变量和集合
+####################################################
+
+# a. 列表 list
+li = [1, 2, 3, 4]
+li[2] #访问
+li = [1, 3, 4, '456', [1, 2, 3], {1: 'one', 2: 'two'}] #元素可以为不同类型
+
+# 操作
+li.append(1)     #最后追加元素
+li.pop()         #尾部删除元素
+li[始:终:step]    #列表切片
+l_a.extend(l_b)  #添加
+
+len(li) #列表长度
+# 遍历
+for i in li:
+    print(i)
+for i in range(len(i)):
+    print(li[i])
+
+# b. 元组 tuple
+# 不可改变的序列
+tup = (1, 2, 3)
+tup[0]   #访问
+tup + (4, 5, 6)    # => (1, 2, 3, 4, 5, 6)
+tup[:2]            # => (1, 2)
+a, b, c = (1, 2, 3)  # 可以把元组合列表解包，赋值给变量
 
 
+# c. 字典 dict
+# 用字典表达映射关系
+
+# 初始化的字典
+d = {'a': 1, 'b' : 2, 1: 'one', 2: 'two', 3: [1, 2, 3]}  #字典是无序的，hash表
+print (d)
+
+# 访问元素
+d['a']
+d[1]
+
+# 字典的遍历
+list(d.keys())     # => ["three", "two", "one"]
+list(d.values())   # => [3, 2, 1]
+
+for key in d:
+    print(d[key])   #遍历值
+for key, value in d.items():
+	print (key, value)  #遍历键，值
+
+# 字典操作
+d.get("one")      # => 1
+d.setdefault("five", 5)  # d["five"]设为5
+d.update({"four":4}) 
+del fd["one"]
+
+# d. 集合 set
+s_a = set([1, 2, 2, 3, 4, 5, 5]) #打印时会自动去重，且无序
+s_b = set([4, 5, 6, 7, 8, 9])
+
+#判断元素是否存在
+5 in s_a
+
+#添加元素
+s_a.add(5) 
+
+#并集
+s_a | s_b
+s_a.union(s_b)
+ 
+#交集
+s_a & s_b
+s_a.intersection(s_b)
+
+#差集 A - (A&B)
+s_a - s_b
+s_a.difference(s_b)
+
+#对称差 (A + B) - (A & B)
+s_a ^ s_b
+s_a.symmetric_difference(s_b)
 
 
+####################################################
+## 3. 流程控制和迭代器
+####################################################
+
+# if else条件语句
+some_var = 5
+if some_var > 10:
+    print("some_var比10大")
+elif some_var < 10:    # elif句是可选的
+    print("some_var比10小")
+else:                  # else也是可选的
+    print("some_var就是10")
+
+# for循环语句遍历列表
+for animal in ["dog", "cat", "mouse"]:
+    print("{} is a mammal".format(animal))
+
+# "range(number)"返回数字列表从0到给的数字
+for i in range(4):
+    print(i)
+
+# while循环直到条件不满足
+x = 0
+while x < 4:
+    print(x)
+    x += 1
+
+# 用try/except块处理异常状况
+try:
+    # 用raise抛出异常
+    raise IndexError("This is an index error")
+except IndexError as e:
+    pass    # pass是无操作，但是应该在这里处理错误
+except (TypeError, NameError):
+    pass    # 可以同时处理不同类的错误
+else:   # else语句是可选的，必须在所有的except之后
+    print("All good!")   # 只有当try运行完没有错误的时候这句才会运行
+
+
+####################################################
+## 4. 函数
+####################################################
+
+# 用def定义新函数
+def add(x, y):
+    print("x is {} and y is {}".format(x, y))
+    return x + y    # 用return语句返回
+
+# 调用函数
+add(5, 6)   # => 印出"x is 5 and y is 6"并且返回11
+
+# 也可以用关键字参数来调用函数
+add(y=6, x=5)   # 关键字参数可以用任何顺序
+
+
+# 我们可以定义一个可变参数函数
+def varargs(*args):
+    return args
+
+varargs(1, 2, 3)   # => (1, 2, 3)
+
+
+# 我们也可以定义一个关键字可变参数函数
+def keyword_args(**kwargs):
+    return kwargs
+
+# 我们来看看结果是什么：
+keyword_args(big="foot", loch="ness")   # => {"big": "foot", "loch": "ness"}
+
+
+# 这两种可变参数可以混着用
+def all_the_args(*args, **kwargs):
+    print(args)
+    print(kwargs)
+"""
+all_the_args(1, 2, a=3, b=4) prints:
+    (1, 2)
+    {"a": 3, "b": 4}
+"""
+
+# 调用可变参数函数时可以做跟上面相反的，用*展开序列，用**展开字典。
+args = (1, 2, 3, 4)
+kwargs = {"a": 3, "b": 4}
+all_the_args(*args)   # 相当于 foo(1, 2, 3, 4)
+all_the_args(**kwargs)   # 相当于 foo(a=3, b=4)
+all_the_args(*args, **kwargs)   # 相当于 foo(1, 2, 3, 4, a=3, b=4)
+
+
+# 函数作用域
+x = 5
+
+def setX(num):
+    # 局部作用域的x和全局域的x是不同的
+    x = num # => 43
+    print (x) # => 43
+
+def setGlobalX(num):
+    global x
+    print (x) # => 5
+    x = num # 现在全局域的x被赋值
+    print (x) # => 6
+
+setX(43)
+setGlobalX(6)
+
+
+def create_adder(x):
+    def adder(y):
+        return x + y
+    return adder
+
+add_10 = create_adder(10)
+add_10(3)   # => 13
+
+# 也有匿名函数
+(lambda x: x > 2)(3)   # => True
+
+# 内置的高阶函数
+map(add_10, [1, 2, 3])   # => [11, 12, 13]
+filter(lambda x: x > 5, [3, 4, 5, 6, 7])   # => [6, 7]
+
+# 用列表推导式可以简化映射和过滤。列表推导式的返回值是另一个列表。
+[add_10(i) for i in [1, 2, 3]]  # => [11, 12, 13]
+[x for x in [3, 4, 5, 6, 7] if x > 5]   # => [6, 7]
+
+
+####################################################
+## 5. 模块
+####################################################
+
+# 用import导入模块
+import math
+print(math.sqrt(16))  # => 4.0
+
+# 也可以从模块中导入个别值
+from math import ceil, floor
+print(ceil(3.7))  # => 4.0
+print(floor(3.7))   # => 3.0
+
+# 可以导入一个模块中所有值
+# 警告：不建议这么做
+from math import *
+
+# 如此缩写模块名字
+import math as m
+math.sqrt(16) == m.sqrt(16)   # => True
+
+# Python模块其实就是普通的Python文件。你可以自己写，然后导入，
+# 模块的名字就是文件的名字。
+
+# 你可以这样列出一个模块里所有的值
+import math
+dir(math)
 
 
