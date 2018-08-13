6afd0b282af70023ce86a21a4419195f7e03d3e1
williamz <wufan_zhao@163.com>
2018-08-13 14:55:42 +0800

    test_message

---
+++ python/basic_py.py
@@ -0,0 +1,24 @@
+# 版本Python 3.7
+## 1. 原始数据类型和运算符
+
+# 模除
+7 % 3 # => 1
+
+# x的y次方
+2**4 # => 16
+
+# 格式化字符串
+"{} can be {}".format("strings", "interpolated")
+#=> "strings can be interpolated"
+
+# 重复参数
+"{0} can be nimble, {0} be quick, {0} jump over the {1}".format("Jack", "candle stick")
+#=> "Jack be nimble, Jack be quick, Jack jump over the candle stick"
+
+
+
+
+
+
+
+
